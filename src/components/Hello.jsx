c58e85e0325472c1623731bc2b2aa99e72272581
abdelrhman <abdelrhmanroshdy8@gmail.com>
2024-09-07 00:03:26 +0300

    Add user authentication feature and update login UI

---
+++ src/components/Hello.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Component} from 'react';
+
+class Hello extends React.Component {
+    render() {
+        return (
+            <>
+                <h1>Hello</h1>
+            </>
+        )
+    }
+}
+
+export default Hello
